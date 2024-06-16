d54a3c8e8c7a18d127a5dc0f1e030103fcfb99f6
jjwang <10402488@qq.com>
2024-06-16 23:06:41 +0800

    Improve pci implementation and add reference website

--- kernel/sys/pci.d
+++
@@ -1,4 +0,0 @@
-sys/pci.o: sys/pci.c sys/pci.h sys/cpu.h sys/panic.h base/klog.h \
- device/display/term.h device/display/fb.h 3rd-party/boot/limine.h \
- device/display/font.h base/image.h base/lock.h base/vector.h \
- base/kmalloc.h
