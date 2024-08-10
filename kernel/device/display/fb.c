af6f4c872384ba4715ba1eff059033d65f1b9741
jjwang <10402488@qq.com>
2024-08-10 17:44:04 +0800

    mm: separate mm.c into 2 files - pmm.c and vmm.c

--- kernel/device/display/fb.c
+++ kernel/device/display/fb.c
@@ -45,18 +45,10 @@ bool fb_set_bg_image(fb_info_t *fb, image_t *img)
 
     memcpy(&(fb->img_bg), img, sizeof(image_t));
     if (fb->bgbuffer == NULL) {
-        fb->bgbuffer = (uint8_t*)kmalloc_chunk(fb->width * fb->height * 4,
-                                               __func__, __LINE__);
-        vmm_map(NULL, (uint64_t)fb->bgbuffer, VIRT_TO_PHYS(fb->bgbuffer),
-                NUM_PAGES(fb->width * fb->height * 4),
-                VMM_FLAGS_DEFAULT);
+        fb->bgbuffer = (uint8_t*)kmalloc(fb->width * fb->height * 4);
     }
     if (fb->swapbuffer == NULL) {
-        fb->swapbuffer = (uint8_t*)kmalloc_chunk(fb->width * fb->height * 4,
-                                                 __func__, __LINE__);
-        vmm_map(NULL, (uint64_t)fb->swapbuffer, VIRT_TO_PHYS(fb->swapbuffer),
-                NUM_PAGES(fb->width * fb->height * 4),
-                VMM_FLAGS_DEFAULT);
+        fb->swapbuffer = (uint8_t*)kmalloc(fb->width * fb->height * 4);
     }
 
     for (size_t y = 0; y < fb->height; y++) {
@@ -191,11 +183,7 @@ void fb_init(fb_info_t *fb, struct limine_framebuffer* s)
 {
     if(s == NULL) {
         if((uint64_t)fb->addr == (uint64_t)fb->backbuffer) {
-            fb->backbuffer = kmalloc_chunk(fb->backbuffer_len,
-                                           __func__, __LINE__);
-            vmm_map(NULL, (uint64_t)fb->backbuffer, VIRT_TO_PHYS(fb->backbuffer),
-                    NUM_PAGES(fb->backbuffer_len),
-                    VMM_FLAGS_DEFAULT);
+            fb->backbuffer = kmalloc(fb->backbuffer_len);
             memcpy(fb->backbuffer, fb->addr, fb->backbuffer_len);
         }
         return;
